6b82b706465875fa474c4901bfc1f7720192a999
Robert Shubert <robs@apexsecurityint.com>
2024-12-18 13:44:31 +0000

    Add files via upload
    Signed-off-by: Robert Shubert <robs@apexsecurityint.com>

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome to the SentinelOps Project"
+
+@app.route('/test')
+def test():
+    return "This is a test route"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
